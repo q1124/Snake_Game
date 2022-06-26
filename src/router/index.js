f3a45eee4afd2d1e177f4608f662fd8144198af1
betty <BettyQiu@fareastone.com>
2022-06-26 23:58:31 +0800

    pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,18 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 import Snake from '../views/index.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/snackGame',
-    name: 'Snake',
-    component: Snake
-  }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
-]
 
-const router = new VueRouter({
-  routes
+export default new Router({
+  mode: 'hash',
+  base: '/',
+  routes: [
+    {
+      path: '/snackGame/',
+      name: 'Snake',
+      component: Snake
+    }
+  ]
 })
-
-export default router
